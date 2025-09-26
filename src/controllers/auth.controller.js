549faeedd452b4e295a9c6d5e6cf0c91df07aaea
Developer2022004 <kiritosword921@gmail.com>
2025-09-26 01:20:29 -0600

    Carga de rutas y tokens de validacion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,25 +1,60 @@
+import { createAccessToken } from "../lib/jwt.js";
+import userModel from "../models/user.model.js";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req,res) => {
     const {username,email,password,codigoAcuaponico} = req.body;
     try{
+
+        //Encriptamos la password
+
+        const passwordHash = await bcrypt.hash(password,10);
+
         const newUser = new User({
             username,
             email,
-            password,
+            password: passwordHash,
             codigoAcuaponico
         });
 
         const userSaved = await newUser.save();
-        // if()
-        res.status(200).json({
-            id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createAt: userSaved.createdAt,
-            updateAt: userSaved.updatedAt
-        });
+
+        const token = await createAccessToken({id:userSaved._id});
+        res.cookie('token',token);
+
+        res.status(200).send("Usuario registrado");
+
+        // res.status(200).json({
+        //     id: userSaved._id,
+        //     username: userSaved.username,
+        //     email: userSaved.email,
+        //     createAt: userSaved.createdAt,
+        //     updateAt: userSaved.updatedAt
+        // });
     }catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
+
+export const login = async (req,res) => {
+    const {email,password} = req.body;
+
+    try{
+        const userFound = await userModel.findOne({email});
+        if(!userFound) return res.status(400).send("Usuario no encontrado");
+
+        const isMatch = await bcrypt.compare(password,userFound.password);
+        if(!isMatch) return res.status(400).send("Credenciales incorrectas");
+
+        const token = await createAccessToken({id:userFound._id});
+
+        res.cookie('token',token);
+
+        res.send("Inicio de sesion correcto");
+
+        //consultado el usuario, comparamos las credenciales
+    }catch(error){
         res.status(500).json({message: error.message});
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import {connectDB} from './db.js';
 
 connectDB();
 
-app.listen(3001);
+app.listen(3001,'0.0.0.0'); //especificamos el puerto y el servidor 0.0.0.0 para escuchar lo externo
 console.log('Servidor en el puerto',3001);
+
